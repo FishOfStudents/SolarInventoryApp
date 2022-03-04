902b5bbb2e5ae80f8dbf58eaa662ab84f9dad716
jamescritt <100893290+jamescritt@users.noreply.github.com>
2022-03-03 20:45:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-'AKfycbzbbQ6sXFixNS04FIUz6quY5xzPMSJEIbYvB4cxT0Ei5Z9jB9RDunQzYjH7JW4V1Fg5eQ'
+'AKfycbwMOGidKDjNLeM-G3dyneKGHqpoh_PoarIAw5HqwvEu0UuOcghuHle51Y_RpNVcIrQNrg'
 )}
