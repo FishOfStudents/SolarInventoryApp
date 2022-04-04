b244a1a84508b7a864a17f3e347349b142cfd060
FishOfStudents <99696956+FishOfStudents@users.noreply.github.com>
2022-04-04 16:31:08 -0600

    Update config.js
    Updated deployment ID since we updated Airtable Google App script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-'AKfycbyGKFmkhOeQTe-cYNhrARFKtjjX6lwkJbrQSJ0AA7CEyoD26B9q9OlygCjb5M0SGHb2iw'
+'AKfycbx86R6OGFhs5w6OYF-4k3E81w2m2rMetTlz81MP2GFhzA9uYTvs0XEku2i-DT06ZCp2mg'
 )}
