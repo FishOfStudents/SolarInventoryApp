9fab6b9747dd64f4e4457b849c4732169ec94a94
FishOfStudents <99696956+FishOfStudents@users.noreply.github.com>
2022-04-22 14:24:28 -0600

    Update config.js
    updated api key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-'AKfycbx86R6OGFhs5w6OYF-4k3E81w2m2rMetTlz81MP2GFhzA9uYTvs0XEku2i-DT06ZCp2mg'
+'AKfycbzMgOpzzgW-d4yJZ-JkBn2Rr_sEhvMOiot7qImE_8qMoZgTS-nVUvhrxWiPYRY6x3SQgA'
 )}
